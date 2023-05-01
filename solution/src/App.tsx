2de1bda545f55faa36cac5617bf04e64ee262640
DW Desktop <westerlund.da@gmail.com>
2023-05-01 17:00:22 +0200

    Init commit all set up with vite, tailwind, react, typescript

---
+++ solution/src/App.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <>
+      <h1>Tjo bre whatup?</h1>
+      <main>
+        <p>All good ye</p>
+      </main>
+    </>
+  );
+};
+
+export default App;
